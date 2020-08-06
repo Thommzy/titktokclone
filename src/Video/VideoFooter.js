074e4c527c3b30d8fcce81fc7b12d7464579be66
Thommzy <tomiwaobeisun@yahoo.com>
2020-08-06 18:29:00 +0100

    videoFooter

--- src/Video/VideoFooter.js
+++ src/Video/VideoFooter.js
@@ -4,7 +4,10 @@ import "./Videos.css";
 function VideoFooter() {
   return (
     <div className="videoFooter">
-      <div className="videoFooter__text"></div>
+      <div className="videoFooter__text">
+        <h3>@korede_bello</h3>
+        <p>This is a description</p>
+      </div>
       <img src="https://static.thenounproject.com/png/934821-200.png" alt="" />
     </div>
   );
